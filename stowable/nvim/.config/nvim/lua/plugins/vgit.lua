4c86002cc5880fdd9a22e0b9c061fab091963ed8
aurum77 <58880138+aurum77@users.noreply.github.com>
2025-05-19 14:56:58 +0300

    move from gitsigns to vgit

---
+++ stowable/nvim/.config/nvim/lua/plugins/vgit.lua
@@ -0,0 +1,23 @@
+return {
+	"tanvirtin/vgit.nvim",
+	init = function()
+		local opts = require("plugins.lsp.opts").opts
+		vim.keymap.set("n", "<Leader>td", "<Cmd>VGit buffer_diff_preview<CR>", opts)
+	end,
+	dependencies = { "nvim-lua/plenary.nvim", "nvim-tree/nvim-web-devicons" },
+	event = "VimEnter",
+	opts = {
+		settings = {
+			hls = {
+				GitAppBar = "GruvboxFg0",
+				GitHeader = "GruvboxFg0",
+			},
+			scene = {
+				diff_preference = "split", -- unified or split
+				keymaps = {
+					quit = "q",
+				},
+			},
+		},
+	},
+}

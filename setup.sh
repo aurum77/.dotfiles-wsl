f1d85f8c4c0b678762cf7e1f4f00d7654ff7bab6
aurum77 <58880138+aurum77@users.noreply.github.com>
2026-04-14 00:50:35 +0300

    fix setup errors and add tree-sitter-cli

--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
+sudo pacman -Syu fakeroot debugedit
 sh ./scripts/update_install.sh
 sh ./scripts/paru.sh
+sh ./install_packages.sh
 sh ./scripts/services.sh
 sh ./scripts/npm_pkg.sh
 sh ./scripts/tpm.sh
